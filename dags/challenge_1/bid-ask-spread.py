044e9353332012c24f3d7c9e41436be7ffc4694a
Laura Santacruz <smlaura97@gmail.com>
2024-06-07 23:09:37 -0600

    add files

---
+++ dags/challenge_1/bid-ask-spread.py
@@ -0,0 +1,38 @@
+'''
+DAG to get monitor the bid-ask spread from order books 
+Created: June 7th 2024
+'''
+from airflow import DAG   
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
+import requests
+import json
+import pendulum
+
+# schedule to run every 10 minutes
+SCHEDULE_INTERVAL = "*/10 * * * *"
+
+default_args = {
+    'owner': 'Laura',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 6, 6, tzinfo=pendulum.timezone('America/Mexico_City')),
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'max_active_runs': 1,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=4),
+    "catchup": False,
+    "trigger_rule": 'all_success'
+}
+
+with DAG(   
+    dag_id="bid ask spread",
+    default_args=default_args,
+    description='pipeline',
+    schedule_interval=SCHEDULE_INTERVAL
+) as dag:
+    # dummies
+    init = DummyOperator(task_id='init')
+
+    
+
