6ba1f0f2d497445bebbeb9f7a8e4c6805a5a840d
Laura Santacruz <smlaura97@gmail.com>
2024-06-09 00:20:09 -0600

    add date_dim sql

---
+++ dags/scripts/sql/date_dim.sql
@@ -0,0 +1,19 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS bitso_de_challenge.date_dim(
+    date_id int, 
+    date date, 
+    day int,
+    month int,
+    year int, 
+    weekday string, 
+    quarter string
+  )
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+  'serialization.format' = ',',
+  'field.delim' = ','
+)
+STORED AS TEXTFILE
+LOCATION 's3://bitso-challenge-output/dim/'
+TBLPROPERTIES (
+  'skip.header.line.count' = '1'
+);
